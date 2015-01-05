9208e9b9c15872f718d69c03540ba663827a563b
Sébastien Piquemal <sebpiq@gmail.com>
2015-01-05 14:11:53 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf8
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+  name='pychedelic',
+  version='0.1.0',
+  description='A clean, beautiful and shiny python library for sound and music.',
+  author='Sébastien Piquemal',
+  author_email='sebpiq@gmail.com',
+  url='https://github.com/sebpiq/pychedelic/',
+  packages=['pychedelic', 'pychedelic.core'],
+)
